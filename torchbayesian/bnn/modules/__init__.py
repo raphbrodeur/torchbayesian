d788d92cc0c3411dd02290cc0d31a0f6e33438e0
Raphaël Brodeur <106177338+raphbrodeur@users.noreply.github.com>
2026-01-15 13:27:45 -0500

    feat: add MCD dropout layers

--- torchbayesian/bnn/modules/__init__.py
+++ torchbayesian/bnn/modules/__init__.py
@@ -1,1 +1,9 @@
 from torchbayesian.bnn.modules.bayesian_module import BayesianModule
+from torchbayesian.bnn.modules.dropout import (
+    BayesianAlphaDropout,
+    BayesianDropout,
+    BayesianDropout1d,
+    BayesianDropout2d,
+    BayesianDropout3d,
+    BayesianFeatureAlphaDropout
+)
